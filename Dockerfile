4940bbbbd1e887d1fdf6465874c1f88497c6b7b3
Jhessica Pinedo <jhessicapinedo@gmail.com>
2025-08-27 15:53:19 -0500

    first commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+# Usa una imagen base de PHP
+FROM php:8.1-apache
+
+# Instala las dependencias necesarias para PostgreSQL
+RUN apt-get update && apt-get install -y \
+    libpq-dev \
+    && docker-php-ext-install pdo pdo_pgsql pgsql
+
+# Copia el contenido de tu aplicación al contenedor
+COPY . /var/www/html/
+
+# Expone el puerto 80
+EXPOSE 80
--- conexion.php
+++ conexion.php
@@ -2,11 +2,11 @@
 
 	function conexion(){
 
-	$host = "host=xxxx";
-	$port = "port=xxxx";
-	$dbname = "dbname=xxxx";
-	$user = "user=xxxx";
-	$password = "password=xxxx";
+	$host = "host=@dpg-d2nmba8gjchc73dkdkh0-a.oregon-postgres.render.com";
+	$port = "port=5432";
+	$dbname = "dbname=examplenubedb";
+	$user = "user=examplenubedb_user";
+	$password = "password=zEHHy52Y6rBEdicHZ46kCfzwyTd0hr0Q";
 
 	$db = pg_connect("$host $port $dbname $user $password");
 
